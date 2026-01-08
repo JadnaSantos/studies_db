80f7d8b70214e8e4b2d70379139128d64f77f045
Jadna Santos <jadnajesus7@gmail.com>
2026-01-08 17:21:59 -0300

    feat: select table

---
+++ scripts/4-select-database.sql
@@ -0,0 +1,9 @@
+\c aulasql
+
+-- Selecionando manualmente todos os valores da coluna
+-- SELECT id, first_name, last_name, email, created_at
+-- FROM customers;
+
+
+-- Selecionando todas as colunas como elas estão na tabela, (nao recomendado)
+SELECT * FROM customers ORDER BY first_name;
